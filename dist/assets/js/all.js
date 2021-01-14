824204938e75c6b87254aac552446634e7981305
koihsun <sag.coi.1215@gmail.com>
2021-01-14 16:28:22 +0800

    version 19

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -108,33 +108,16 @@ $('#videoModal').on('hide.bs.modal', function (e) {
 $(document).on('click', '.js-videoPoster', function (ev) {
   ev.preventDefault();
   var $poster = $(this);
-  var $wrapper = $poster.closest('.js-videoWrapper');
-  videoPlay($wrapper);
+  var $wrapper = $('.js-videoWrapper');
+  videoPlay($wrapper, $poster);
 }); // play the targeted video (and hide the poster frame)
 
-function videoPlay($wrapper) {
+function videoPlay($wrapper, $poster) {
   var $iframe = $wrapper.find('.js-videoIframe');
-  var src = $iframe.data('src'); // hide poster
-
-  $wrapper.addClass('videoWrapperActive'); // add iframe src in, starting the video
+  var src = $poster.data('src'); // hide poster
+  // add iframe src in, starting the video
 
   $iframe.attr('src', src);
-} // stop the targeted/all videos (and re-instate the poster frames)
-
-
-function videoStop($wrapper) {
-  // if we're stopping all videos on page
-  if (!$wrapper) {
-    var $wrapper = $('.js-videoWrapper');
-    var $iframe = $('.js-videoIframe'); // if we're stopping a particular video
-  } else {
-    var $iframe = $wrapper.find('.js-videoIframe');
-  } // reveal poster
-
-
-  $wrapper.removeClass('videoWrapperActive'); // remove youtube link, stopping the video from playing in the background
-
-  $iframe.attr('src', '');
 } //isotope
 
 
